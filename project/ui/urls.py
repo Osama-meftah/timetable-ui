d98f406bcab9a799a45705993772350663fce2ab
Osama <osamameftah900@gmail.com>
2025-07-24 00:25:15 +0300

    merge with test

--- project/ui/urls.py
+++ project/ui/urls.py
@@ -41,10 +41,7 @@ urlpatterns = [
     path('departments/add/', views.DepartmentCreateView.as_view(), name='add_department'),
     path('departments/edit/<int:id>/', views.DepartmentUpdateView.as_view(), name='edit_department'),
     path('departments/delete/<int:id>/', views.DepartmentDeleteView.as_view(), name='delete_department'),
-    
-    # path('program/add/', views.AddAndEditProgramView.as_view(), name='add_program'),
-    # path('program/edit/<int:id>/', views.AddAndEditProgramView.as_view(), name='edit_program'),
-    
+   
     path('programs/add/', views.AddProgramLevelView.as_view(), name='add_program'),
     path('programs/edit/<int:id>/',views.EditProgramLevelView.as_view(), name='edit_program'),
     path('programs/delete/<int:id>/',views.DeleteProgramLevelView.as_view(), name='delete_program'),
